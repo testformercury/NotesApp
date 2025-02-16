8e2b60071aea59e7ef64290c47e1fc6809ec5139
testformercury <152368454+testformercury@users.noreply.github.com>
2025-02-16 19:19:26 +0530

    Update Sync.tsx

--- database/Sync.tsx
+++ database/Sync.tsx
@@ -3,21 +3,17 @@ import { getUnsyncedNotes, syncNotes } from "./Db";
 
 export const syncDataWithServer = async () => {
     const netInfo = await NetInfo.fetch();
-    console.log(netInfo.isConnected);
     if (!netInfo.isConnected) {
-        console.log("nointernet");
         return;
     }
 
     const unsyncedItems = await getUnsyncedNotes();
 
     if (unsyncedItems.length === 0) {
-        console.log("nosyncneeded");
         return;
     }
     try {
         for (const item of unsyncedItems) {
-            console.log("itemsynced");
             await syncNotes(item.id);
         }
     } catch (error) {
